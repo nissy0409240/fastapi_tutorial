fb829cb2377db5b4f22f2e128cf008f5217fec1c
Nissy0409 <i.wish.glory.reds@gmail.com>
2021-01-12 13:27:45 +0900

    Query paramater

---
+++ query_param.py
@@ -0,0 +1,16 @@
+from typing import Optional
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/{item_id}")
+async def read_user_item(
+    item_id: str, needy: str, skip: int = 0, limit: Optional[int] = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
+
